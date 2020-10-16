e15019c3aaebdba590e6e5d4a73533f96aa98226
ICARUS-OOF <kaydenchai2008@gmail.com>
2020-10-16 18:57:33 +0800

    PlayerMovement, Camera, game created

---
+++ Evo Core/Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ProjectFactory.Movement
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        public Transform player;
+        void Update()
+        {
+            transform.position = player.transform.position;
+        }
+    }
+}
